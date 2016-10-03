7ed397e6aa64901c954f60486de8d2bab5d1398c
malder1975 <malder120875@gmail.com>
2016-10-03 12:31:53 +0300

    add static pages model

---
+++ common/models/StaticPages.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%static_pages}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $alias
+ * @property integer $published
+ * @property string $content
+ * @property string $title_browser
+ * @property string $meta_keywords
+ * @property string $meta_description
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class StaticPages extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%static_pages}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'alias'], 'required'],
+            [['published'], 'integer'],
+            [['content'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['title', 'alias', 'title_browser'], 'string', 'max' => 255],
+            [['meta_keywords'], 'string', 'max' => 200],
+            [['meta_description'], 'string', 'max' => 160],
+            [['alias'], 'unique'],
+        ];
+    }
+
+    
+}
